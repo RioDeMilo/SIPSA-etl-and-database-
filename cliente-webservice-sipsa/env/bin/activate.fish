835ad2d39eb94d60609eb1b118975be810e573a9
RioDeMilo <camiloandresdelosrios@gmail.com>
2026-02-24 16:42:33 -0500

    aggretation of a new .py to create the cities database from Python and pandas and exporting it to a sqlite3 database where I already have the schema

--- cliente-webservice-sipsa/env/bin/activate.fish
+++ cliente-webservice-sipsa/env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/milo/SIPSA/cliente-webservice-sipsa/env
+set -gx VIRTUAL_ENV /home/milo/sipsa/cliente-webservice-sipsa/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
